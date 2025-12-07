2c4e1afe0e519aaa2e17262785cc59e637a14fd4
Vidhya-Viswa <vidhyaviswa20@gmail.com>
2025-12-07 23:37:29 +0530

    Modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';  // New homepage component
+import Home from './components/Home'; 
 import Login from './components/Login';
-
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />  // Homepage at root
-        <Route path="/login" element={<Login />} />  // Login at /login
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />  
+        <Route path="/login" element={<Login />} />  
+        {/* Removed Dashboard route because the file was deleted */}
       </Routes>
     </Router>
   );
